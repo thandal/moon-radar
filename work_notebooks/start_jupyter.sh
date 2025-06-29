c8c5092097534425c942040b156dc1ebd00063a4
thandal <than@timbrel.org>
2025-06-29 12:17:18 -0400

    sync

--- work_notebooks/start_jupyter.sh
+++ work_notebooks/start_jupyter.sh
@@ -7,7 +7,7 @@ python3 -m venv python_env
 source python_env/bin/activate
 
 # Install jupyter:
-pip3 install --quiet --upgrade jupyterlab jupyterlab-git
+#pip3 install --quiet --upgrade jupyterlab jupyterlab-git
 
 # Set LD_LIBRARY_PATH for WSL cuda support.
 LD_LIBRARY_PATH=/usr/lib/wsl/lib/ \
